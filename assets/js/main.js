6b29441d52356d1d77529cb3f29adc078aca0cad
itomo8849 <tomolllllllo@gmail.com>
2022-11-11 18:03:06 +0900

    onclick YouTube

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
 window.onload = function () {
   document.getElementById( "remodal-btn" ).click();
 }
+function servVideo(anything){
+  document.querySelector('.js_modalVideo').src = 'https://'+anything;
+};
