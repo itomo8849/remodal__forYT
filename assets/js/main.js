6b84f4374cbd8ea2e3ed9f280ea96db319d40a8f
itomo8849 <tomolllllllo@gmail.com>
2022-11-13 16:14:35 +0900

    switch script

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 window.onload = function () {
   document.getElementById( "remodal-btn" ).click();
 }
-function servVideo(anything){
+function servVideo(anything,clickElement){
   document.querySelector('.js_modalVideo').src = 'https://'+anything;
+  document.getElementsByClassName('js_YTswitchBtn is_active')[0].classList.remove('is_active');
+  clickElement.classList.add('is_active');
 };
